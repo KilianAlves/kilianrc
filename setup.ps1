9f4ac8551be43ae7cb41159bfa98ed00eb664be0
Kilian Alves <47873462+KilianAlves@users.noreply.github.com>
2025-04-11 23:29:20 +0200

    Script dispo sur win et lin

---
+++ setup.ps1
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env sh
+echo --% >/dev/null;: ' | out-null
+<#'
+
+
+#
+# sh part
+#
+echo "hello from bash/dash/zsh"
+echo "do whatver you want just dont use #＞ directly"
+echo "e.g. do #""> or something similar"
+# end bash part
+
+exit #>
+
+
+#
+# powershell part
+#
+echo "hello from powershell"
+echo "you literally don't have to escape anything here"
